6dbf7f70737057e1a62a7eaea034ad425aa3c09e
Mr. Nattakarn Khumsupha <nattakarn.khumsupha.user1@outlook.com>
2026-06-08 19:55:44 +0700

    feat: add solid-styled-components dependency and implement new package routes

---
+++ apps/web/src/routes/packages/query-solid/index.tsx
@@ -0,0 +1,107 @@
+import { Title } from "@solidjs/meta";
+import { useSolidMutation, useSolidQuery } from "@skyjt/query-solid";
+import { action, useAction } from "@solidjs/router";
+
+const delayedServerIncrement = action(async (newValue: number) => {
+  "use server";
+  const delay = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+  await delay(1000); // Simulate a delay of 1 second
+  console.log("Server incremented value:", newValue);
+  return newValue + 1;
+});
+
+export default function Home() {
+  const query = useSolidQuery({
+    queryKey: "count",
+    async queryFn() {
+      return 0;
+    },
+    initialData: 0,
+  });
+
+  const incrementAction = useAction(delayedServerIncrement);
+
+  const mutation = useSolidMutation({
+    mutationFn: async (newValue: number) => {
+      return await incrementAction(newValue);
+    },
+    onSuccess(data) {
+      query.mutate(data);
+      console.log("Client updated value:", data);
+    },
+  });
+
+  return (
+    <>
+      <Title>Solid Start</Title>
+      <h1>Welcome to Solid Start!</h1>
+      <button
+        disabled={mutation.isPending}
+        onClick={() => {
+          mutation.mutate(query.data);
+        }}
+      >
+        {mutation.isPending ? "Incrementing..." : "Increment"}
+      </button>
+      <p>You have clicked the button {query.data} times.</p>
+      <hr></hr>
+      <h2>Code</h2>
+      <pre>
+        {`
+import { Title } from "@solidjs/meta";
+import { useSolidMutation, useSolidQuery } from "@skyjt/query-solid";
+import { action, useAction } from "@solidjs/router";
+
+const delayedServerIncrement = action(async (newValue: number) => {
+  "use server";
+  const delay = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+  await delay(1000); // Simulate a delay of 1 second
+  console.log("Server incremented value:", newValue);
+  return newValue + 1;
+});
+
+export default function Home() {
+  const query = useSolidQuery({
+    queryKey: "count",
+    async queryFn() {
+      return 0;
+    },
+    initialData: 0,
+  });
+
+  const incrementAction = useAction(delayedServerIncrement);
+
+  const mutation = useSolidMutation({
+    mutationFn: async (newValue: number) => {
+      return await incrementAction(newValue);
+    },
+    onSuccess(data) {
+      query.mutate(data);
+      console.log("Client updated value:", data);
+    },
+  });
+
+  return (
+    <>
+      <Title>Solid Start</Title>
+      <h1>Welcome to Solid Start!</h1>
+      <button
+        disabled={mutation.isPending}
+        onClick={() => {
+          mutation.mutate(query.data);
+        }}
+      >
+        {mutation.isPending ? "Incrementing..." : "Increment"}
+      </button>
+      <p>You have clicked the button {query.data} times.</p>
+    </>
+  );
+}
+
+`}
+      </pre>
+    </>
+  );
+}
