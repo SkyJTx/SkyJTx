783c06a43365a56e71ac516ddbb54b3ce665596b
Mr. Nattakarn Khumsupha <nattakarn.khumsupha.user1@outlook.com>
2026-06-11 20:16:11 +0700

    feat: implement initial presentations for contacts and works sections

--- apps/web/src/presentations/contacts/contacts.tsx
+++ apps/web/src/presentations/contacts/contacts.tsx
@@ -2,23 +2,25 @@ import { BrandTitle, BrandSubtitle } from "~/components/Brand";
 import { Box } from "~/components/Box";
 import { useContactsController } from "./contacts.controller";
 import * as S from "./styles";
+import { useTheme } from "solid-styled-components";
 
 /**
  * Contacts section presentation.
  */
 export function ContactsPresentation() {
   const c = useContactsController();
+  const theme = useTheme();
 
   return (
-    <S.SectionWrapper id="Contacts">
+    <S.SectionWrapper theme={theme} id="Contacts">
       <BrandTitle>Get in Touch</BrandTitle>
       <BrandSubtitle>Let's build something beautiful together</BrandSubtitle>
 
       <S.ContentBox>
         <Box>
           <S.PlaceholderWrapper>
-            <S.PlaceholderTitle>Coming Soon</S.PlaceholderTitle>
-            <S.PlaceholderDescription>
+            <S.PlaceholderTitle theme={theme}>Coming Soon</S.PlaceholderTitle>
+            <S.PlaceholderDescription theme={theme}>
               The contact section is currently being updated. Please check back later.
             </S.PlaceholderDescription>
           </S.PlaceholderWrapper>
--- apps/web/src/presentations/works/works.tsx
+++ apps/web/src/presentations/works/works.tsx
@@ -2,23 +2,25 @@ import { BrandTitle, BrandSubtitle } from "~/components/Brand";
 import { Box } from "~/components/Box";
 import { useWorksController } from "./works.controller";
 import * as S from "./styles";
+import { useTheme } from "solid-styled-components";
 
 /**
  * Works presentation highlighting recent engineering and acoustic projects.
  */
 export function WorksPresentation() {
   const c = useWorksController();
+  const theme = useTheme();
 
   return (
-    <S.SectionWrapper id="Works">
+    <S.SectionWrapper theme={theme} id="Works">
       <BrandTitle>My Works</BrandTitle>
       <BrandSubtitle>Selected projects and experiments</BrandSubtitle>
 
-      <S.WorksBoxContainer>
+      <S.WorksBoxContainer theme={theme}>
         <Box>
           <S.PlaceholderWrapper>
-            <S.PlaceholderTitle>Coming Soon</S.PlaceholderTitle>
-            <S.PlaceholderDescription>
+            <S.PlaceholderTitle theme={theme}>Coming Soon</S.PlaceholderTitle>
+            <S.PlaceholderDescription theme={theme}>
               I am currently preparing selected engineering and development projects. Check back soon for updates.
             </S.PlaceholderDescription>
           </S.PlaceholderWrapper>
