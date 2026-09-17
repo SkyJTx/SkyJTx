94c2ead3bdcaa5d1a6fca3384428635fd603e4b1
Mr. Nattakarn Khumsupha <nattakarn.khumsupha.user1@outlook.com>
2026-09-17 21:02:59 +0700

    Refactor code structure for improved readability and maintainability

--- apps/example-fullstack/src/vite-env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="@solidjs/vite-plugin/boundary-modules" />
-/// <reference types="../file-routes.d.ts" />
--- apps/example-tanstack-solid-2/src/vite-env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="filesystem-routing/types" />
-/// <reference types="@solidjs/vite-plugin/boundary-modules" />
